4e339264403341734595543f3c29526f501074cf
HyeonJin127 <guswls041207@naver.com>
2025-04-01 11:52:56 +0900

    Create Section.js

---
+++ HW/0401/component/Section.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Section() {
+  return (
+    <section>
+      <h2>섹션 제목</h2>
+      <p>여기는 섹션 내용입니다. HTML과 CSS를 이용한 페이지 디자인을 연습하고 있습니다.</p>
+    </section>
+  );
+}
+
+export default Section;
