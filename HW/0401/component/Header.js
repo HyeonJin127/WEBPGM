47c30854dde6516e3d78a852bd74cbea5d6ce682
HyeonJin127 <guswls041207@naver.com>
2025-04-01 11:52:38 +0900

    Create Header.js

---
+++ HW/0401/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>환영합니다!</h1>
+      <p>이것은 간단한 HTML과 CSS로 만든 웹페이지입니다.</p>
+    </header>
+  );
+}
+
+export default Header;
