a222450e4b15086218a10a0c52ab4018786460f5
HyeonJin127 <guswls041207@naver.com>
2025-04-01 11:51:57 +0900

    Create App.js

---
+++ HW/0401/component/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Section from './Section';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
